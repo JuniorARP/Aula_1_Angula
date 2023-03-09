97a6303dd65954229954769527f18bdd23e894d5
JuniorARP <55111719+JuniorARP@users.noreply.github.com>
2023-03-09 11:22:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aula_1_Angula
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-x7xhcm)
---
+++ src/me/me.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-me',
+  standalone: true,
+  templateUrl: './me.component.html',
+  styleUrls: ['./me.component.css'],
+})
+export class MeComponent implements OnInit {
+  myName: string;
+  constructor() {
+    this.myName = 'Arthur';
+  }
+  ngOnInit() {}
+}
